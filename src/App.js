6bf5967d5e8a00abc5170f0584c9d3d086c83b16
Nixolas <nicolasza26@gmail.com>
2023-05-18 18:19:56 -0300

    only-login0.7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  //scroll to the top
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  function login() {
+    alert("Login");
+  }
+
+  function createAccount(event) {
+    // event.preventDefault();
+    setMessage(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="login__main-container">
+      <main className="login__main">
+        <header className="login__main-header">
+          <h1 className="login__main-header-title">facebook</h1>
+          <p className="login__main-header-info1">
+            Connect with friends and the world around you on Facebook
+          </p>
+        </header>
+        <form className="login__main-form">
+          <input
+            className="login__main-form-input-username"
+            type="text"
+            placeholder="Email or phone number"
+            onChange={createAccount}
+            value={message}
+          />
+          <input
+            className="login__main-form-input-password"
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            onClick={login}
+            className="login__main-form-input-submit"
+            value="Log In"
+            type="submit"
+          />
+          <a class="login__main-form-a-forgotpassword" href="#">
+            Forgot password?
+          </a>
+          <hr></hr>
+          <button
+            onClick={createAccount}
+            className="login__main-form-button-createaccount"
+          >
+            Create new account
+          </button>
+        </form>
+        <p className="login__main-container-info2">
+          <a href="#">Create a Page</a> for a celebrity, brand or business.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </main>
     </div>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <footer>
+        <ul className="footer__ul-languages">
+          <li>English (US)</li>
+          <li>Español</li>
+          <li>Português (Brasil)</li>
+          <li>Français (France)</li>
+          <li>Italiano</li>
+          <li>Deutsch</li>
+          <li>العربية</li>
+          <li>中文(简体)</li>
+          <li>हिन्दी</li>
+          <li>日本語</li>
+        </ul>
+        <hr />
+        <ul className="footer__ul-about">
+          <li>Sign Up</li>
+          <li>Log In</li>
+          <li>Messenger</li>
+          <li>Facebook Lite</li>
+          <li>Watch</li>
+          <li>Places</li>
+          <li>Games</li>
+          <li>Marketplace</li>
+          <li>Meta Pay</li>
+          <li>Meta Store</li>
+          <li>Meta Quest</li>
+          <li>Instagram</li>
+          <li>Bulletin</li>
+          <li>Fundraisers</li>
+          <li>Services</li>
+          <li>Voting Information Center</li>
+          <li>Privacy Policy</li>
+          <li>Privacy Center</li>
+          <li>Groups</li>
+          <li>About</li>
+          <li>Create Ad</li>
+          <li>Create Page</li>
+          <li>Developers</li>
+          <li>Careers</li>
+          <li>Cookies</li>
+          <li>Ad choices</li>
+          <li>Terms</li>
+          <li>Help</li>
+          <li>Contact Uploading & Non-Users</li>
+        </ul>
+        <span>Meta © 2023</span>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
