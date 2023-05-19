eeb250bcee754cb37eb80efb45adfa43d8b44fc1
Nixolas <nicolasza26@gmail.com>
2023-05-18 21:21:40 -0300

    only-login-fail

--- src/App.js
+++ src/App.js
@@ -25,42 +25,45 @@ function App() {
         <header className="login__main-header">
           <h1 className="login__main-header-title">facebook</h1>
           <p className="login__main-header-info1">
-            Connect with friends and the world around you on Facebook
+            Connect with friends and the world around you on Facebook.
           </p>
         </header>
-        <form className="login__main-form">
-          <input
-            className="login__main-form-input-username"
-            type="text"
-            placeholder="Email or phone number"
-            onChange={createAccount}
-            value={message}
-          />
-          <input
-            className="login__main-form-input-password"
-            type="password"
-            placeholder="Password"
-          />
-          <input
-            onClick={login}
-            className="login__main-form-input-submit"
-            value="Log In"
-            type="submit"
-          />
-          <a class="login__main-form-a-forgotpassword" href="#">
-            Forgot password?
-          </a>
-          <hr></hr>
-          <button
-            onClick={createAccount}
-            className="login__main-form-button-createaccount"
-          >
-            Create new account
-          </button>
-        </form>
-        <p className="login__main-container-info2">
-          <a href="#">Create a Page</a> for a celebrity, brand or business.
-        </p>
+
+        <div className="form-container">
+          <form className="login__main-form">
+            <input
+              className="login__main-form-input-username"
+              type="text"
+              placeholder="Email or phone number"
+              onChange={createAccount}
+              value={message}
+            />
+            <input
+              className="login__main-form-input-password"
+              type="password"
+              placeholder="Password"
+            />
+            <input
+              onClick={login}
+              className="login__main-form-input-submit"
+              value="Log In"
+              type="submit"
+            />
+            <a class="login__main-form-a-forgotpassword" href="#">
+              Forgot password?
+            </a>
+            <hr></hr>
+            <button
+              onClick={createAccount}
+              className="login__main-form-button-createaccount"
+            >
+              Create new account
+            </button>
+          </form>
+          <p className="login__main-container-info2">
+            <a href="#">Create a Page</a> for a celebrity, brand or business.
+          </p>
+        </div>
       </main>
     </div>
   );
