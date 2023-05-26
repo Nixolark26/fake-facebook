9f5562589fa1c907e623c7434d167d13c1b505c3
Nixolas <nicolasza26@gmail.com>
2023-05-26 18:17:23 -0300

    developing-myprofile-2.0

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./home.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../utils/firebase";
 import HomeTopNav from "../../navs/HomeTopNav";
 import HomeLeftNav from "../../navs/HomeLeftNav";
 import HomeRightNav from "../../navs/HomeRightNav";
-import HomePosts from "../home/posts/HomePosts";
+import HomePosts from "./HomePosts";
+import "./home.css";
 
 function Home() {
   const [user, loading] = useAuthState(auth);
